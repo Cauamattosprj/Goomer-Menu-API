ba09b8336530694a50eac9029b8f9ec2c7562215
Caua Mattos <caua.h4k@gmail.com>
2025-10-25 15:48:16 -0300

    chore: add eslint, lint-staged and husky

---
+++ eslint.config.ts
@@ -0,0 +1,56 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+const config = defineConfig([
+  {
+    files: ["**/*.{ts,tsx,js}"],
+    ignores: ["dist", "node_modules"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+
+      "@typescript-eslint/explicit-function-return-type": [
+        "error",
+        { allowExpressions: false },
+      ],
+
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/typedef": [
+        "error",
+        {
+          arrayDestructuring: true,
+          arrowParameter: true,
+          memberVariableDeclaration: true,
+          objectDestructuring: true,
+          parameter: true,
+          propertyDeclaration: true,
+          variableDeclaration: true,
+        },
+      ],
+      "@typescript-eslint/no-unsafe-assignment": "error",
+      "@typescript-eslint/no-unsafe-call": "error",
+      "@typescript-eslint/no-unsafe-member-access": "error",
+
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+    },
+  },
+]);
+
+export default config;

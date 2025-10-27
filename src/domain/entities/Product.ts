0bd3543017dd4455c28d65158ec99ae6c293b3a9
Caua Mattos <caua.h4k@gmail.com>
2025-10-26 21:57:09 -0300

    fix: product class now only have attribute category id

--- src/domain/entities/Product.ts
+++ src/domain/entities/Product.ts
@@ -4,21 +4,21 @@ export class Product {
   private id?: string | undefined;
   private name: string;
   private price: number;
-  private category: string;
+  private categoryId?: string | null;
   private visible: boolean = true;
 
   constructor(params: {
     id?: string;
     name: string;
     price: number;
-    category: string;
+    categoryId?: string | undefined | null;
     visible?: boolean;
   }) {
     this.id = params.id;
     this.id ??= uuidv4();
     this.name = params.name;
     this.price = params.price;
-    this.category = params.category;
+    this.categoryId = params.categoryId;
     this.visible = params.visible ?? true;
   }
 
@@ -32,8 +32,8 @@ export class Product {
   getPrice(): number {
     return this.price;
   }
-  getCategory(): string {
-    return this.category;
+  getCategoryId(): string | undefined | null {
+    return this.categoryId;
   }
   isVisible(): boolean {
     return this.visible;
@@ -47,8 +47,8 @@ export class Product {
     if (price < 0) throw new Error("Price cannot be negative");
     this.price = price;
   }
-  setCategory(category: string): void {
-    this.category = category;
+  setCategoryId(category: string): void {
+    this.categoryId = category;
   }
   setVisible(visible: boolean): void {
     this.visible = visible;

95fc8fe124330104add748d5d782913959a9ed27
Caua Mattos <caua.h4k@gmail.com>
2025-10-26 21:52:29 -0300

    chore: add a new method on product repository port

--- src/ports/repository/ProductRepositoryPort.ts
+++ src/ports/repository/ProductRepositoryPort.ts
@@ -1,9 +1,10 @@
-import type { Product } from "@/src/domain/entities/Product";
+import { Product } from "@/domain/entities/Product";
 
 
 export interface ProductRepositoryPort {
   findById(id: string): Promise<Product | null>;
   findAll(): Promise<Product[]>;
+  findByName(name: string): Promise<Product | null>
   save(product: Product): Promise<void>;
   delete(id: string): Promise<void>;
 }

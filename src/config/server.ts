43847f004be3763ec18c3c74ebcac578a8ca8277
Caua Mattos <caua.h4k@gmail.com>
2025-10-27 21:46:02 -0300

    feat: refactor server index to include category and promotion routers

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,18 +1,19 @@
 import express from "express";
-import logger from "../shared/middleware/loggerMiddleware";
-import { productRouter } from "../adapters/rest/ProductController";
-import 'dotenv/config';
+import { productRouter } from "@/adapters/rest/ProductController";
+import { categoryRouter } from "@/adapters/rest/CategoryController";
+import { promotionRouter } from "@/adapters/rest/PromotionController";
 import { menuRouter } from "@/adapters/rest/MenuController";
 
-
+const port = 3000
 const app = express();
-const port: number = 3000;
 
-app.use(logger);
 app.use(express.json());
-
 app.use("/api", productRouter);
+app.use("/api", categoryRouter);
+app.use("/api", promotionRouter);
 app.use("/api", menuRouter);
+
+export { app };
 
 app.listen(port, () => {
   console.log(`Groomer Menu API listening on port ${port}`);
